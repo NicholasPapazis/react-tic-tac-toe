fa9c8fa97b951157e09b42abeada9ea1b1705c0f
Nicholas Papazis <nicholasepapazis@gmail.com>
2025-05-31 11:51:12 -0400

    comment starter code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+//code in this file creates a component, a piece of reusable code that represents part of a UI
+//components are used to render, manage, and update the UI elements in your application
+
+
+// function square() -> defines function called square
+// export -> makes this function accessible outside of this file
+// default -> tells other files using this code that it's the main function in this file
 export default function Square() {
+  
+  // returns a button
+  // <button> is a JSX element (JSX is combo of JS and HTML that describes what you want to display)
+  // className = "square" -> a button prop that tells CSS how to style the button
+  // X -> the text displayed inside of the button
   return <button className="square">X</button>;
+  
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+//The bridge between the component created in App.js and the web browser
 
-import App from "./App";
+//the below 4 lines bring all the necessary pieces together
+import { StrictMode } from "react"; //React
+import { createRoot } from "react-dom/client"; //React's library to talk to web browsers (React DOM)
+import "./styles.css"; // the styles for the components
 
+import App from "./App"; // the component created in App.js
+
+
+
+//injects the final product into public/index.html 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
